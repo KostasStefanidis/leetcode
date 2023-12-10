d6ae12065577111465389cc7b6fecb5cbbadf509
kstef <kstefanidis48@gmail.com>
2023-12-10 15:00:18 +0200

    9. Palindrome number solution

---
+++ 9. Palindrome Number/main.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        # Converting the integer to string and then to a list
+        # to use the builtin .reverse() method
+        # list_x = list(str(x))
+        # reverse_list_x = list_x.copy()
+        # reverse_list_x.reverse()
+        # return reverse_list_x == list_x
+        
+        # without converting the integer to string
+        if x < 0:
+            return False
+
+        reverse_x: int = 0
+        temp: int = x
+        
+        while (temp != 0):
+            digit = temp % 10
+            reverse_x = reverse_x * 10 + digit
+            temp = temp // 10
+
+        return reverse_x == x
